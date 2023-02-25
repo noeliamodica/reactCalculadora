266e9c7f82c5ecc1529858702e0978d4d2b207f2
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-02-25 19:28:58 -0300

    armado css de contenedor y botones calculadora

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 
 import './App.css';
+import Logo from './imagenes/calculadora.png'
+import Boton from './componentes/Boton';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      
+      <div className='logo-contenedor'>
+        <img
+          src={Logo}
+          className='logo'
+          alt='Logo' />
+      </div>
+
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+          <Boton>1</Boton>
+          <Boton>4</Boton>
+          <Boton>7</Boton>
+          <Boton>=</Boton>
+
+        <div className='fila'></div>
+          <Boton>2</Boton>
+          <Boton>5</Boton>
+          <Boton>8</Boton>
+          <Boton>0</Boton>
+        <div className='fila'></div>
+          <Boton>3</Boton>
+          <Boton>6</Boton>
+          <Boton>9</Boton>
+          <Boton>.</Boton>
+        <div className='fila'></div>
+          <Boton>+</Boton>
+          <Boton>-</Boton>
+          <Boton>*</Boton>
+          <Boton>/</Boton>
+        <div className='fila'></div>
+      </div>
+
      
     </div>
   );
